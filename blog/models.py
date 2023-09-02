f665e95df3fd2413643a997a8c8bdfa9d9485259
Alexander <Lisnevskiy14@gmail.com>
2023-09-02 23:57:45 +0300

    Implemented blog functionality

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250, verbose_name='заголовок')
+    content = models.TextField(verbose_name='содержание')
+    image = models.ImageField(upload_to='blogs/', verbose_name='изображение')
+    views_count = models.PositiveIntegerField(default=0, verbose_name='количество просмотров')
+    publication_date = models.DateField(auto_now_add=True, verbose_name='дата публикации')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
